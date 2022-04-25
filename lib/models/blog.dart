2c0d62f577c61ee526117cad069f9449ba45a115
Brian Ding <brian.ding@sobrian.com>
2022-04-25 20:58:49 +0800

    feat: add blog model

---
+++ lib/models/blog.dart
@@ -0,0 +1,10 @@
+class Blog {
+  late int id;
+  late String title;
+  late int authorID;
+  late String path;
+  late String graphID;
+  late String wechatUrl;
+  late DateTime created;
+  late DateTime modified;
+}
