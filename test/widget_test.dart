3618373c8a775bf77f19eab20f9f7ebe85452269
Brian Ding <brian_ding@outlook.com>
2022-04-28 13:47:11 +0800

    test: udpate package

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:web-sobrian/main.dart';
+import 'package:web_sobrian/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
